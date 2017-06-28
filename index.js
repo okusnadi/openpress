d408467dd51bfd2597c85b40b57f05ddde2a0676
EmpireWorld <hasanbayat1393@gmail.com>
2017-06-28 21:21:41 +0430

    Init

--- index.js
+++ index.js
@@ -1,56 +1,12 @@
-const fs = require('fs');
-const url = require('url');
-const path = require('path');
-const http = require('http');
-const https = require('https');
-const mime = require('mime');
-const plugin = require('./lib/plugin');
+const server = require('./lib/server');
+const pkg = require('./package.json');
 
-const server = http.createServer((request, response) => {
-  response.setHeader('X-Powered-By', 'OpenPress');
-  response.setHeader('Content-Language', 'en');
-  response.setHeader('Cache-Control', 'no-cache');
-});
-
-server.listen(8080, 'localhost', 0, () => {
-  console.log('Server is Listening on port 8080');
-});
-
-server.on('request', (request, response) => {
-  let filePath = path.join(__dirname, decodeURI(request.url));
-  response.setHeader('Content-Type', mime.lookup(filePath));
-  fs.stat(filePath, (err, stats) => {
-    if (err) {
-      response.end(err.toString());
-      return;
-    }
-    if (stats.isFile()) {
-      fs.readFile(filePath, (err, data) => {
-        if (err) {
-          response.end(err.toString());
-          return;
-        }
-        response.write(data);
-        response.end();
-      });
-      return;
-    } else if (stats.isDirectory()) {
-      fs.readdir(filePath, (err, files) => {
-        if (err) {
-          response.end(err.toString());
-          return;
-        }
-        response.write(`${request.url}\n`);
-        for (let file of files) {
-          response.write(`\t/${file}\n`);
-        }
-        response.end();
-        return;
-      });
-      return;
-    } else {
-      response.end(err.toString());
-      return;
-    }
-  });
-});
+module.exports = {
+  name: pkg.name,
+  version: pkg.version,
+  description: pkg.description,
+  author: pkg.author,
+  start() {
+    server.start();
+  }
+};
